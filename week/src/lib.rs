818b3d7b03d7055366c82873aaeef1b04aa1a395
Billy Tobon <billy.tobon@gmail.com>
2024-02-22 16:18:10 +0100

    Allows multiple formats

--- week/src/lib.rs
+++ week/src/lib.rs
@@ -2,7 +2,8 @@ use chrono::{Datelike, NaiveDate, ParseError};
 
 pub fn week(str_date: &str) -> Result<u32, ParseError> {
 
-    let iso_week = NaiveDate::parse_from_str(str_date, "%Y-%m-%d")?.iso_week();
+    let iso_week = NaiveDate::parse_from_str(str_date, "%Y-%m-%d").
+    or(NaiveDate::parse_from_str(str_date, "%Y/%m/%d"))?.iso_week();
     Ok(iso_week.week())
  
  }
@@ -12,21 +13,17 @@ pub fn week(str_date: &str) -> Result<u32, ParseError> {
 #[cfg(test)]
 mod tests {
 
-    use chrono::NaiveDate;
-
     use crate::week;
 
+
     #[test]
-    fn test_parse() {
-        
-        let parse_from_str = NaiveDate::parse_from_str;
-        assert_eq!(parse_from_str("2015-09-05", "%Y-%m-%d"),
-               Ok(NaiveDate::from_ymd_opt(2015, 9, 5).unwrap()));
+    fn test_week_slashes(){
+        assert_eq!(week("2023/02/19"), Ok(7));
 
     }
 
     #[test]
-    fn test_week(){
+    fn test_week_dashes(){
         assert_eq!(week("2023-02-19"), Ok(7));
 
     }
